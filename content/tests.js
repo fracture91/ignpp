7654ebb522d9b75f24e7e2271d580996a5551cb7
fracture91 <fracture91@gmail.com>
2010-12-20 21:14:59 -0500

    Added test file with some tests for the cases where line breaks don't come out correctly in the parser

---
+++ content/tests.js
@@ -0,0 +1,95 @@
+/* 
+You can paste the contents of this file into the console to run the tests.
+The extension never runs this code on its own.
+*/
+
+(function(){
+
+function Test(name, expectedOutput, func) {
+
+	this.name = name;
+	this.expectedOutput = expectedOutput;
+	this.func = func;
+	
+	this.run = function() {
+		var rv = this.func();
+		var pass = this.expectedOutput == rv;
+		return {passed: pass, output: rv};
+	}
+
+}
+
+var Tester = new function() {
+	
+	var tests = [];
+	
+	this.add = function(test) {
+		tests.push(test);
+	}
+	
+	this.run = function() {
+		
+		var results = "";
+		for(var i=0, len=tests.length; i<len; i++) {
+			var thisTest = tests[i];
+			var testResult = thisTest.run();
+			if(testResult.passed) {
+				results += "*** " + thisTest.name + " passed";
+			}
+			else {
+				results += "!!! " + thisTest.name + " failed\nExpected Output:\n" + thisTest.expectedOutput + "\nActual Output:\n" + testResult.output;
+			}
+			results += "\n";
+		}
+
+		return results;
+
+	}
+	
+}
+
+
+
+//////////Actual tests start here
+
+
+
+Tester.add(new Test("simple parse", "a", function(){
+	return Parse.HTML("a");
+}));
+
+Tester.add(new Test("text separated by one br", "a\na", function(){
+	return Parse.HTML("a<br/>a");
+}));
+
+Tester.add(new Test("text separated by two br", "a\n\na", function(){
+	return Parse.HTML("a<br/><br/>a");
+}));
+
+Tester.add(new Test("divs separated by zero br", "a\na", function(){
+	return Parse.HTML("<div>a</div><div>a</div>");
+}));
+
+Tester.add(new Test("divs separated by one br", "a\n\na", function(){
+	return Parse.HTML("<div>a</div><br/><div>a</div>");
+}));
+
+Tester.add(new Test("divs separated by two br", "a\n\n\na", function(){
+	return Parse.HTML("<div>a</div><br/><br/><div>a</div>");
+}));
+
+Tester.add(new Test("divs separated by one div wrapped br", "a\n\na", function(){
+	return Parse.HTML("<div>a</div><div><br/></div><div>a</div>");
+}));
+
+Tester.add(new Test("divs separated by two div wrapped br", "a\n\n\na", function(){
+	return Parse.HTML("<div>a</div><div><br/></div><div><br/></div><div>a</div>");
+}));
+
+
+
+//////////Actual tests end here
+	
+return Tester.run();
+
+})();
