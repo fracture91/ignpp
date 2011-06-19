e0a0dff236363413fc4e640352a9ac9ed013877d
fracture91 <fracture91@gmail.com>
2011-06-19 03:40:17 -0400

    #215 and #216 - Usercolors options are now sufficiently pretty

--- content/options/usercolorcontroller.js
+++ content/options/usercolorcontroller.js
@@ -287,7 +287,7 @@ usercolorController.prototype = {
 		//this will handle validation of styles as well
 		var that = this;
 		vestitools_style.postColors(username, styles, function(xhr, success, username, styles) {
-				that.buttonLog("post", success ? "Colors successfully posted and saved." : that.XHRErrorMessage(xhr));
+				that.buttonLog("post", success ? "Personal colors successfully posted and saved." : that.XHRErrorMessage(xhr));
 				vestitools_style.defaultPostColorsCallback(xhr, success, username, styles);
 				//set the inputs to the now-validated values
 				if(success) {
